6974efa1e9b67302d06059a710d58a462477ffe7
Volodymyr_Trach <vovka.trach@gmail.com>
2020-02-17 02:08:14 +0200

    cleared commented code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(15);
-  // const [postsPerPage] = useState(15);
 
 
   useEffect(() => {
