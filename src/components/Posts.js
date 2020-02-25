f5fd0c08441102036cfcf91a948f46afee059cc1
oksanalutsyk <oksanalutsyk95@gmail.com>
2020-02-25 12:28:21 +0200

    first Commit

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ProductsItem from './ProductsItem';
 import './productsItem.css';
 
+// import SearchBar from './SearchBar';
+
+
 export const Posts = ({ posts }) => {
 
     return (
         <div className='posts'>
+            {/* <SearchBar /> */}
             {posts.map(post => (
                 <ProductsItem keyID={post.id} post={post.title}></ProductsItem>
             ))}
